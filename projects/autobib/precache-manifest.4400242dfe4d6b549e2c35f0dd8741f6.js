0d0da452bed3d8cbd25913bd250dba4fe7b1883a
Wong-Innovations <dylanwong@nevada.unr.edu>
2021-08-24 17:26:02 -0700

    updated projects

---
+++ projects/autobib/precache-manifest.4400242dfe4d6b549e2c35f0dd8741f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265b4e8fc4f7decd304f49379ed161b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faaa9fca4bcbdc04f7a9",
+    "url": "/static/css/main.e4bfcca6.chunk.css"
+  },
+  {
+    "revision": "49ac9960e281603fdaee",
+    "url": "/static/js/2.77033343.chunk.js"
+  },
+  {
+    "revision": "faaa9fca4bcbdc04f7a9",
+    "url": "/static/js/main.f487d62e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
